1ad0f33434d8886883a017faa556f15fc24e940e
rajatk-31 <rajat.yadav@zenways.io>
2018-02-22 19:57:52 +0530

    ok

--- routes/creatTodo.js
+++ routes/creatTodo.js
@@ -2,14 +2,14 @@ var dbTask = require('../models/todo')
 
 
 exports.createTodo = (req,res)=>{
-    if(!req.body.email||!req.body.name){
+    if(!req.body.name){
         res.json({
             success: false,
             msg: "Please provide all details"
         })
     }else{
         var newTask = new dbTask({
-            email: req.body.email,
+            email: req.decoded.email,
             name: req.body.name
         })
         newTask.save((err, data)=>{
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,16 @@
 var express = require('express')
 var router =express.Router();
+var tokenVerify = require('./tokenVerify')
+
 
 var createTodo = require('./creatTodo')
-router.post('/createTodo',createTodo.createTodo)
+router.post('/createTodo',tokenVerify.tokenVerify,createTodo.createTodo)
 
+var registration = require('./registration')
+router.post('/register', registration.registration)
+
+var login = require('./login')
+router.post('/login', login.login)
 
 
 module.exports = router;
